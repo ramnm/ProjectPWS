cf8fc09cb8a7a0a836f64e43cbc4ffcc1eed88d4
Jared Casale <jared.casale@gmail.com>
2015-03-15 16:31:20 -0700

    CMD Check error fixing - Moved all data to sysdata.rda inside the R folder. Added docs for parameters of read and save functions.

--- R/readPWSTable.R
+++ R/readPWSTable.R
@@ -2,6 +2,7 @@
 #' @author Maruthi Ram Nadakuduru
 #' @description This function reads a saved PWS table created by user from
 #'              the specified location.
+#' @param fileName Name of file where saved PWStation object exists.
 #' @export
 readPWSTable <- function(fileName){
   readRDS(fileName)
--- R/savePWSTable.R
+++ R/savePWSTable.R
@@ -1,6 +1,8 @@
 #' Save PWS Table
 #' @author Maruthi Ram Nadakuduru
-#' @description This function saves the PWS table created by user in the specified location
+#' @description This function saves the PWS table created by user in the specified location.
+#' @param pwsTable The PWStation object to save.
+#' @param fileName The location where it is to be saved.
 #' @export
 savePWSTable <- function(pwsTable, fileName) {
   saveRDS(pwsTable, file = fileName)
